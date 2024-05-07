51e3c17487f5250384b328c481dda1dce6d53c05
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-05-07 23:40:10 +0500

    add get union key utils

---
+++ projects/ng-thrift/src/lib/utils/get-union-key.ts
@@ -0,0 +1,22 @@
+import get from 'lodash-es/get';
+import isNil from 'lodash-es/isNil';
+import isObject from 'lodash-es/isObject';
+import { ValuesType } from 'utility-types';
+
+export function getUnionKeys<T>(obj: T): (keyof T)[] {
+    return obj ? (Object.keys(obj) as (keyof T)[]) : [];
+}
+
+export function getUnionKeyValue<T>(obj: T): { [K in keyof T]: [K, T[K]] }[keyof T] | null {
+    return isObject(obj)
+        ? (Object.entries(obj).find(([, v]) => !isNil(v)) as { [K in keyof T]: [K, T[K]] }[keyof T])
+        : null;
+}
+
+export function getUnionKey<T>(obj: T): keyof T | null {
+    return get(getUnionKeyValue(obj), 0, null);
+}
+
+export function getUnionValue<T extends object>(obj: T): ValuesType<T> | null {
+    return get(getUnionKeyValue(obj), 1, null) as never;
+}
--- projects/ng-thrift/src/lib/utils/index.ts
+++ projects/ng-thrift/src/lib/utils/index.ts
@@ -2,3 +2,4 @@ export * from './namespace-type';
 export * from './to-json';
 export * from './create-union';
 export * from './is-equal-thrift';
+export * from './get-union-key';
