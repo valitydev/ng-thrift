f62c39fe04c2a95a5852f030881fa0665a34b244
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-06-21 13:44:58 +0500

    TD-925: Bump Angular to 18 (#12)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-    extends: '@vality/eslint-config',
+    extends: '@vality/eslint-config-ng',
     overrides: [
-        ...require('@vality/eslint-config/configs').angular('ngt').overrides,
-        ...require('@vality/eslint-config/configs').importOrder(['@ngt/**']).overrides,
+        ...require('@vality/eslint-config-ng/configs').angular('ngt').overrides,
+        ...require('@vality/eslint-config-ng/configs').importOrder(['@ngt/**']).overrides,
     ],
 };
